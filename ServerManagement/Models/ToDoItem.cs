9f50665487e08e667e0f541879f17f4f99328d75
Administrator <gitlab_admin_d5ab0c@example.com>
2024-09-15 19:48:19 +0300

    Chapted 4 course project (part 1) my version done

---
+++ ServerManagement/Models/ToDoItem.cs
@@ -0,0 +1,35 @@
+﻿namespace ServerManagement.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        private string _body;
+
+        public string Body
+        {
+            get { return _body; }
+            set
+            {
+                _body = value;
+                UpdatedAt = DateTime.UtcNow;
+            }
+        }
+
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                _isDone = value;
+                UpdatedAt = DateTime.UtcNow;
+            }
+        }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ ServerManagement/Models/ToDoItemsRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace ServerManagement.Models
+{
+    public static class ToDoItemsRepository
+    {
+        private static List<ToDoItem> _toDoItems { get; set; } =
+        [
+            new ToDoItem() { Id = 5, Body = "To Do item 5" },
+            new ToDoItem() { Id = 4, Body = "To Do item 4" },
+            new ToDoItem() { Id = 3, Body = "To Do item 3" },
+            new ToDoItem() { Id = 2, Body = "To Do item 2" },
+            new ToDoItem() { Id = 1, Body = "To Do item 1" },
+        ];
+
+        private static void SortItems()
+        {
+            _toDoItems.Sort((x, y) => y.Id.CompareTo(x.Id));
+        }
+
+        public static List<ToDoItem> GetSortedToDoItems()
+        {
+            return _toDoItems;
+        }
+
+        //public static void UpdateToDoItem(ToDoItem item)
+        //{
+        //    ToDoItem toDoItem = _toDoItems.FirstOrDefault(i => i.Id == item.Id);
+        //    if (toDoItem == null) return;
+
+        //    toDoItem.Body = item.Body;
+        //    toDoItem.IsDone = item.IsDone;
+        //    toDoItem.UpdatedAt = DateTime.UtcNow;
+        //}
+
+        public static void AddToDoItem()
+        {
+            _toDoItems.Insert(0, new ToDoItem()
+            {
+                Id = _toDoItems.Max(x => x.Id) + 1,
+                Body = $"New Task {_toDoItems.Max(x => x.Id) + 1}"
+            });
+        }
+    }
+}
